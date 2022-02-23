31dd258f8c5e2e9fa5a4d2d25509f6d6d2c507dc
Kasper <kw2011@live.dk>
2022-02-23 13:17:10 +0100

    Removed useNavigate

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { RandomItemCollection, RandomItem } from "./generator/itemGenerator";
 import './stylesheet.css';
 import { removeTimeoutItem } from './components/cartList';
